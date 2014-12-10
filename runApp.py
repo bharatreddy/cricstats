291dcd15640d015991c0474baf4c761316e30c65
Bharat Kunduri <bharatr@vt.edu>
2014-12-09 23:53:44 -0800

    more graph iterations

--- runApp.py
+++ runApp.py
@@ -17,11 +17,10 @@ def dataBatsman():
     batsmanSmryRet = db.store_result().fetch_row( maxrows=0 )
     batsmandet = [ s for s in batsmanSmryRet ]
     return json.dumps( [ { 'Name': batsmandet[r][0], 'Runs':batsmandet[r][1], \
-    	'Balls':batsmandet[r][2],'StrikeRate':batsmandet[r][3], 'Matches':batsmandet[r][4] }
+        'Balls':batsmandet[r][2],'StrikeRate':batsmandet[r][3], 'Matches':batsmandet[r][4] }
          for r in range( len( batsmandet ) ) ] )
 
 @app.route("/dataBowler")
-#, userDict=userD3List
 def dataBowler():
     # Query the database for Bowler stats
     queryBowler = """
@@ -32,8 +31,22 @@ def dataBowler():
     bowlerSmryRet = db.store_result().fetch_row( maxrows=0 )
     bowlerdet = [ s for s in bowlerSmryRet ]
     return json.dumps( [ { 'Name': bowlerdet[r][0], 'Runs':bowlerdet[r][1], 'Balls':bowlerdet[r][2], \
-    	'RunRate':bowlerdet[r][3],'Matches':bowlerdet[r][4], 'Wickets':bowlerdet[r][5] }
+        'RunRate':bowlerdet[r][3],'Matches':bowlerdet[r][4], 'Wickets':bowlerdet[r][5] }
          for r in range( len( bowlerdet ) ) ] )
+
+@app.route("/dataGraph")
+def dataGraph():
+    # Query the database for Bowler stats
+    queryGraph = """
+            SELECT Batsman, Bowler, StrikeRate, Matches, Dismissed
+            FROM PlayerGraph
+            """
+    db.query( queryGraph )
+    graphSmryRet = db.store_result().fetch_row( maxrows=0 )
+    graphdet = [ s for s in graphSmryRet ]
+    return json.dumps( [ { 'Batsman': graphdet[r][0], 'Bowler':graphdet[r][1], 'StrikeRate':graphdet[r][2], \
+        'Matches':graphdet[r][3],'Dismissed':graphdet[r][4] }
+         for r in range( len( graphdet ) ) ] )
 
 @app.route("/")
 def hello():
--- static/plyrgraph.js
+++ static/plyrgraph.js
@@ -1,47 +1,64 @@
+// Written by Bharat Kunduri
+// Based on the D3 Scatter Plot Example --> http://bl.ocks.org/weiglemc/6185069
+var callback = function (dataGraph) {
+  
+  var dataset = new Array ();
+  for (var i = 0; i < dataBatsman.length; i++) {
+    dataset[i] = [ dataBatsman[i]['Batsman'], dataBatsman[i]['Bowler'], 
+    dataBatsman[i]['StrikeRate'], dataBatsman[i]['Matches'], dataBatsman[i]['Dismissed'] ];
+  }
+
 var width = 960,
-    height = 500;
+    height = 500
 
-var color = d3.scale.category20();
-
-var force = d3.layout.force()
-    .charge(-120)
-    .linkDistance(30)
-    .size([width, height]);
-
-var svg = d3.select("body").append("svg")
+var svg = d3.select("#d3plotBatsman").append("svg")
     .attr("width", width)
     .attr("height", height);
 
-d3.json("miserables.json", function(error, graph) {
-  force
-      .nodes(graph.nodes)
-      .links(graph.links)
-      .start();
+var force = d3.layout.force()
+    .gravity(.05)
+    .distance(100)
+    .charge(-100)
+    .size([width, height]);  
+
+force.nodes(json.nodes)
+     .links(json.links)
+     .start();
 
   var link = svg.selectAll(".link")
-      .data(graph.links)
+      .data(json.links)
     .enter().append("line")
-      .attr("class", "link")
-      .style("stroke-width", function(d) { return Math.sqrt(d.value); });
+      .attr("class", "link");
 
   var node = svg.selectAll(".node")
-      .data(graph.nodes)
-    .enter().append("circle")
+      .data(json.nodes)
+    .enter().append("g")
       .attr("class", "node")
-      .attr("r", 5)
-      .style("fill", function(d) { return color(d.group); })
       .call(force.drag);
 
-  node.append("title")
-      .text(function(d) { return d.name; });
+  // node.append("image")
+  //     .attr("xlink:href", "https://github.com/favicon.ico")
+  //     .attr("x", -8)
+  //     .attr("y", -8)
+  //     .attr("width", 16)
+  //     .attr("height", 16);
+
+  // node.append("text")
+  //     .attr("dx", 12)
+  //     .attr("dy", ".35em")
+  //     .text(function(d) { return d.name });
 
   force.on("tick", function() {
-    link.attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
+    link.attr("x1", function(d) { return d.Batsman.x; })
+        .attr("y1", function(d) { return d.Batsman.y; })
+        .attr("x2", function(d) { return d.Bowler.x; })
+        .attr("y2", function(d) { return d.Bowler.y; });
 
-    node.attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
-  });
-});
+    node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+};
+d3.json("/dataGraph", callback);
+dataset = callback;
+// window.open('www.yourdomain.com','_blank');
+// var data = [[5,3], [10,17], [15,4], [2,8]];
+// console.log( data )
